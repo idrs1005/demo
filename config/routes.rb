d7903d65022852212d7ba72249491e9c5fbd7e53
Isaac <danielramso_12@hotmail.com>
2016-02-10 10:59:41 -0600

    Update rubymine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get 'publicacion' => 'publicacion#index'
+  get 'publicacion/new' => 'publicacion#new'
+  post 'publicacion/create' => 'publicacion#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
