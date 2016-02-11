3b51e4f8ade3251584d49107c93a456d905bd843
Isaac <danielramso_12@hotmail.com>
2016-02-11 12:32:48 -0600

    Rubymine - Registrar un usuario y rutas

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get 'publicacion/new' => 'publicacion#new'
   post 'publicacion/create' => 'publicacion#create'
 
+  get 'register' => 'register#index'
+  post 'register/create' => 'register#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
