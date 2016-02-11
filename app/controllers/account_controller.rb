5e8720d1d7d3d729b3b40ccc9b2b153bb7bf4329
Isaac <danielramso_12@hotmail.com>
2016-02-11 16:50:45 -0600

    Rubymine - Parametros como post

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,12 +1,17 @@
 class AccountController < ApplicationController
 
+  require "uri"
+  require "net/http"
+
   def index
   end
 
   def login
     @user = Paciente.find_by(usuario: params[:account][:usuario], password: params[:account][:password])
     if @user != nil
-      redirect_to account_patient_path(@user)
+      params = @user
+      x = Net::HTTP.post_form(URI.parse('http://192.168.1.129:3000/account/patient'), params)
+      puts x.body
     else
       render 'index' , notice: 'Invalid email/password combination'
     end
