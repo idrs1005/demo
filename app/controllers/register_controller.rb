b2e311804c235c02a03024346423596379698948
Isaac <danielramso_12@hotmail.com>
2016-02-11 12:08:40 -0600

    Rubymine - Registrar un usuario

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,4 +1,12 @@
 class RegisterController < ApplicationController
   def index
+    @usuario = Usuario.new
   end
+
+  def create
+    @usuario = Usuario.new(params[:register].permit(:nombre, :apellido1, :apellido2, :email, :usuario, :password))
+    @usuario.save
+    redirect_to register_path    # esta ruta se explica a continuación
+  end
+
 end
