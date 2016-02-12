b41b4462f500947dac60e460aafaac66a25a1126
Isaac <danielramso_12@hotmail.com>
2016-02-12 08:49:11 -0600

    Rubymine - Link sin id 5

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get 'account' => 'account#index'
   #get 'account/patient', to: 'account#patient'
-  get 'account/patient', to: :patient
+  get 'account/patient', to: :patient, controller: 'account'
   post 'account/login' => 'account#login'
 
   #resource :account
