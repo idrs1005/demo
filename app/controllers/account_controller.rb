758618f04fb9c8fe844276ca791238c329c011bd
Isaac <danielramso_12@hotmail.com>
2016-02-12 10:29:24 -0600

    Rubymine - Editar perfil

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,17 +1,9 @@
 class AccountController < ApplicationController
 
-
-
   def index
   end
 
-  def login
-    @user = Paciente.find_by(usuario: params[:account][:usuario], password: params[:account][:password])
-    if @user != nil
-      redirect_to account_patient_path(@user)
-    else
-      render 'index' , notice: 'Invalid email/password combination'
-    end
+  def edit
   end
 
   def patient
