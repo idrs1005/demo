c36d3e158980c295329b5b1a4fa07c86127d7b81
Isaac <danielramso_12@hotmail.com>
2016-02-16 09:25:53 -0600

    Rubymine - Log in

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -12,7 +12,7 @@ class AccountController < ApplicationController
     @patient = Paciente.find_by(usuario: params[:account][:usuario], password: params[:account][:password])
     if @patient != nil
       @user = @patient
-      @session = Session.new(params.permit(:authenticity_token, [:account][:usuario]))
+      @session = Session.new(params.permit(:authenticity_token, @paciente.usuario))
       @session.save
       render 'patient'
     else
