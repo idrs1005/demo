1088f06592f4bea17d3a279aecd154990eb2d472
Isaac <danielramso_12@hotmail.com>
2016-02-16 08:40:11 -0600

    Rubymine - Log in

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -9,14 +9,14 @@ class AccountController < ApplicationController
   end
 
   def patient
-    @patient = Paciente.find_by(usuario: params[:account][:usuario], password: params[:account][:password])
-    if @patient != nil
-      @user = @patient
-      @session = Session.new(params.permit(:authenticity_token, :@patient.id))
+    #@patient = Paciente.find_by(usuario: params[:account][:usuario], password: params[:account][:password])
+    #if @patient != nil
+     # @user = @patient
+      @session = Session.new(params.permit(:authenticity_token, 1))
       @session.save
-    else
-      render 'index' , notice: 'Invalid email/password combination'
-    end
+    #else
+     # render 'index' , notice: 'Invalid email/password combination'
+    #end
   end
 
 end
