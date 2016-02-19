fbdd483d4ad2c9bc7c8af9a9d6512c2de0b1e673
Isaac <danielramso_12@hotmail.com>
2016-02-19 12:00:06 -0600

    Rubymine - Enviando emails

--- app/mailers/action_correo.rb
+++ app/mailers/action_correo.rb
@@ -1,2 +1,9 @@
 class ActionCorreo < ApplicationMailer
+  default from: 'isaac@flecharoja.com'
+
+  def bienvenido_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Enviando correos con ruby')
+  end
+
 end
