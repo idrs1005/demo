3f62c5577354e907f4e2c768e105cd5d427f6b06
Isaac <danielramso_12@hotmail.com>
2016-02-29 09:53:36 -0600

    Rubymine - Editando atributos de la BD

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -10,8 +10,12 @@ class AccountController < ApplicationController
 
   def update
     @patient = Paciente.find(session[:current_user_id])
-    #mydate = DateTime.new(
-    if @patient.update(usuarios: params[:account][:usuario])
+    mydate = fecha_nacimiento
+    sex = 'Femenino'
+    if sexo
+        sex = 'Masculino'
+    end
+    if @patient.update(nombre: nombre, apellido1: nombre, apellido2: apellido2, identificacion: identificacion, ocupacion: ocupacion, sexo: sex)
       render 'patient'
     else
       render 'edit'
@@ -27,6 +31,36 @@ class AccountController < ApplicationController
       flash[:error] = "Invalid user/email combination"
       render 'index'
     end
+  end
+
+  private
+
+  def sexo
+      params[:account][:autolyse]
+  end
+
+  def fecha_nacimiento
+    DateTime.new(params[:account]["fecha_nacimiento(1i)"] ,params[:account]["fecha_nacimiento(2i)"],params[:account]["fecha_nacimiento(13i)"])
+  end
+
+  def nombre
+    params[:account][:nombre]
+  end
+
+  def apellido1
+    params[:account][:apellido1]
+  end
+
+  def apellido2
+    params[:account][:apellido2]
+  end
+
+  def identificacion
+    params[:account][:identificacion]
+  end
+
+  def ocupacion
+    params[:account][:ocupacion]
   end
 
 end
