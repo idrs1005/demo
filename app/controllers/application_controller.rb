2998b81615186b56f402df636474a3f0829bdd77
Isaac <danielramso_12@hotmail.com>
2016-02-29 10:31:18 -0600

    Rubymine - Editando atributos de la BD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @_current_user ||= session[:current_user_id] &&
-        User.find_by(id: session[:current_user_id])
+        Paciente.find_by(id: session[:current_user_id])
   end
 
 end
